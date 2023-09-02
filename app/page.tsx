e17f87f533e2ffc087d472571add27323b5f70d2
Mim-Sky <horodyski.michal@gmail.com>
2023-09-02 19:30:54 +0000

    Removed projects.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import WorkExpirience from "../components/WorkExpirience";
-import Projects from "../components/Projects";
+// import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
 import { motion, useReducedMotion, MotionConfig } from "framer-motion";
 import Link from 'next/link'
