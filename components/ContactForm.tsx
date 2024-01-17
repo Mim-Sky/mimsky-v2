89bab3d7e6df61d8f0fb48799a44420a44e61f70
Mim-Sky <horodyski.michal@gmail.com>
2024-01-17 17:08:21 +0000

    Accesibility updates: <label> added to ContactForm

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,46 +1,51 @@
-import React from 'react'
-import { useForm, ValidationError } from '@formspree/react';
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
-type Props = {}
+type Props = {};
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("mnqkvdyd");
   if (state.succeeded) {
-      return <p className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg">Thank you!</p> ;
+    return (
+      <p className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg">
+        Thank you!
+      </p>
+    );
   }
   return (
-      <form onSubmit={handleSubmit}
+    <form
+      onSubmit={handleSubmit}
       className="flex flex-col space-y-3 w-fit mx-auto"
-       >
+    >
+      <label htmlFor="email" className="hidden">
+        Email
+      </label>
       <input
         className="contactInput"
         placeholder="Email"
         id="email"
-        type="email" 
+        type="email"
         name="email"
       />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <label htmlFor="message" className="hidden">
+        Message
+      </label>
       <textarea
         className="contactInput"
         placeholder="Message"
         id="message"
         name="message"
       />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" 
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <button
+        type="submit"
         disabled={state.submitting}
-        className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg hover:text-whiteText">
+        className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg hover:text-whiteText"
+      >
         Submit
       </button>
     </form>
   );
 }
-export default ContactForm
+export default ContactForm;
